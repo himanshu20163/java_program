aa08cdf3d21d832321efb316c4571a0e509fb58c
himanshu20163 <109578416+himanshu20163@users.noreply.github.com>
2022-08-03 11:46:27 +0530

    updated

---
+++ hello_java.java
@@ -0,0 +1,10 @@
+public class hello_java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Geekster 1");
+        System.out.println("Hello Geekster 2");
+        System.out.print("Hello Geekster 3");
+        System.out.println("Hello Geekster 4");
+        System.out.print("Hello Geekster 5");
+    }
+}
