6ab00ebed93ed30aa608c9fb4614fab7c7d96b55
himanshu20163 <109578416+himanshu20163@users.noreply.github.com>
2022-08-03 12:00:05 +0530

    two sum number program file

---
+++ WAP_sum_of_two_numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class WAP_sum_of_two_numbers {
+     
+    public static Scanner scn = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.print("Enter First Number");
+        int n = scn.nextInt();//input statment
+        System.out.print("Enter Second Number");
+        int m = scn.nextInt();
+        int z= n+m;//store statement
+        System.out.print("Sum of Two Number" +" ");
+        System.out.print(z);//print statment
+    }
+}
